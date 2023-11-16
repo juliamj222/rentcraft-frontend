1d388fa79d07e2a56991a37516860be59b5f9a2f
julia menendez jardon <juliamenendezjardon@gmail.com>
2023-11-16 11:20:25 -0500

    select works

--- src/components/payments-section/PaymentsCreate.jsx
+++ src/components/payments-section/PaymentsCreate.jsx
@@ -1,144 +1,98 @@
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
-import React, { useState, useEffect } from 'react';
-import { API_PAYMENTS_CREATE, API_TENANTS_VIEW_ALL, API_UNIT_VIEW_BY_USER } from "../constants/endpoints";
-
-// let unitInfo = [
-//   {
-//       "_id": "6553c8c4ee87c365de7582b2",
-//       "user_id": "654bf00e29feba843cd679d5",
-//       "address": "177 Main Street",
-//       "city": "Winooski",
-//       "state": "Vt ",
-//       "zip": "05404",
-//       "monthlyRent": 2000,
-//       "unitState": "Vacant",
-//       "__v": 0
-//   },
-//   {
-//       "_id": "6553c8d5ee87c365de7582b5",
-//       "user_id": "654bf00e29feba843cd679d5",
-//       "address": "2268 Main Street",
-//       "city": "Burlington",
-//       "state": "Vt",
-//       "zip": "05404",
-//       "monthlyRent": 7000,
-//       "unitState": "Vacant",
-//       "__v": 0
-//   },
-//   {
-//       "_id": "6555179b95675976c2984d6a",
-//       "user_id": "654bf00e29feba843cd679d5",
-//       "address": "Hello",
-//       "city": "",
-//       "state": "",
-//       "zip": "",
-//       "monthlyRent": null,
-//       "unitState": "",
-//       "__v": 0
-//   },
-//   {
-//       "_id": "6555185c95675976c2984d70",
-//       "user_id": "654bf00e29feba843cd679d5",
-//       "address": "Home",
-//       "city": "",
-//       "state": "",
-//       "zip": "",
-//       "monthlyRent": null,
-//       "unitState": "",
-//       "__v": 0
-//   }
-// ]
+import React, { useState, useEffect } from "react";
+import {
+  API_PAYMENTS_CREATE,
+  API_TENANTS_VIEW_ALL,
+  API_UNIT_VIEW_BY_USER,
+} from "../constants/endpoints";
 
 function PaymentsCreate(props) {
-
   const [unit_id, setUnit_id] = useState("");
   const [tenant_id, setTenant_id] = useState("");
   const [date, setDate] = useState("");
   const [amount, setAmount] = useState("");
   const [paymentState, setPaymentState] = useState("");
-  
+
   // functions to populate dropdown
   const [unitData, setUnitData] = useState([]);
   const [tenantData, setTenantData] = useState([]);
 
   async function fetchUnits() {
-    
     try {
-
       // Headers
-      const myHeaders = new Headers()
-      myHeaders.append("Authorization", props.token)
+      const myHeaders = new Headers();
+      myHeaders.append("Authorization", props.token);
 
       // Request Options
       let requestOptions = {
         method: "GET",
         headers: myHeaders,
-      }
+      };
 
       // Send Request
-      const response = await fetch(API_UNIT_VIEW_BY_USER + "/" + props.currentId, requestOptions)
+      const response = await fetch(
+        API_UNIT_VIEW_BY_USER + "/" + props.currentId,
+        requestOptions
+      );
 
       // Get a Response
-      const data = await response.json()
-      console.log(data)
-      
+      const data = await response.json();
+      console.log(data);
+
       // Set State
-      setUnitData(data.user_units)
-      
+      setUnitData(data.user_units);
+      if (data.user_units.length > 0) {
+        setUnit_id(data.user_units[0]._id);
+      }
     } catch (error) {
-
-      console.error(error)
-      
+      console.error(error);
     }
-
   }
 
   async function fetchTenants() {
-    
     try {
-
       // Headers
-      const myHeaders = new Headers()
-      myHeaders.append("Authorization", props.token)
+      const myHeaders = new Headers();
+      myHeaders.append("Authorization", props.token);
 
       // Request Options
       let requestOptions = {
         method: "GET",
-        headers: myHeaders
-      }
+        headers: myHeaders,
+      };
 
       // Send Request
-      const response = await fetch(API_TENANTS_VIEW_ALL + "/" + props.currentId, requestOptions)
+      const response = await fetch(
+        API_TENANTS_VIEW_ALL + "/" + props.currentId,
+        requestOptions
+      );
 
       // Get a Response
-      const data = await response.json()
-      console.log(data)
+      const data = await response.json();
+      console.log(data);
 
       // Set State
-      setTenantData(data.user_tenants)
-      
+      setTenantData(data.user_tenants);
+      if (data.user_tenants.length > 0) {
+        setTenant_id(data.user_tenants[0]._id);
+      }
     } catch (error) {
-
-      console.error(error)
-      
+      console.error(error);
     }
-
   }
 
   useEffect(() => {
     if (!props.token) return;
-    fetchUnits()
-    fetchTenants()
+    fetchUnits();
+    fetchTenants();
   }, [props.token]);
 
   async function handleSubmit() {
-    
     try {
-
       // Headers
-      const myHeaders = new Headers()
-      myHeaders.append("Content-Type", "application/json")
-      myHeaders.append("Authorization", props.token)
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("Authorization", props.token);
 
       // Body
       let body = {
@@ -146,45 +100,41 @@ function PaymentsCreate(props) {
         tenant_id: tenant_id,
         date: date,
         amount: amount,
-        paymentState: paymentState
-      }
+        paymentState: paymentState,
+      };
 
       // Request Options
       const requestOptions = {
         method: "POST",
         headers: myHeaders,
-        body: JSON.stringify(body)
-      }
+        body: JSON.stringify(body),
+      };
 
       // Send Request
-      const response = await fetch(API_PAYMENTS_CREATE, requestOptions)
+      const response = await fetch(API_PAYMENTS_CREATE, requestOptions);
 
       // Get a response
-      const data = await response.json()
-      
+      const data = await response.json();
+
       // Refresh the payments feed
-      props.fetchPaymentsFeed()
-      console.log(data)
-      
+      props.fetchPaymentsFeed();
+      console.log(data);
     } catch (error) {
-
-      console.error(error)
-      
+      console.error(error);
     }
-
   }
+  if (!unit_id) return <h1>We don't have a unit registered</h1>;
+  if (!tenant_id) return <h1>We don't have a tenant registered</h1>;
 
   return (
     <>
       <div>
-
         <h4>Create a payment</h4>
 
         <Form>
-
           {/* Select Unit */}
           <FormGroup>
-            <Label for='selectUnit'>Select Unit</Label>
+            <Label for="selectUnit">Select Unit</Label>
             <Input
               id="selectUnit"
               name="selectUnit"
@@ -193,14 +143,19 @@ function PaymentsCreate(props) {
               value={unit_id}
               onChange={(e) => setUnit_id(e.target.value)}
             >
-              {unitData.map((unit, index) => <option key={index} value={unit._id}>{unit.address}</option>)}
+              <option>Select unit</option>
+              {unitData.map((unit, index) => (
+                <option key={index} value={unit._id}>
+                  {unit.address}
+                </option>
+              ))}
             </Input>
           </FormGroup>
 
           {/* Select Tenant */}
           <FormGroup>
-            <Label for='selectTenant'>Select Tenant</Label>
-            <Input 
+            <Label for="selectTenant">Select Tenant</Label>
+            <Input
               id="selectTenant"
               name="selectTenant"
               type="select"
@@ -208,31 +163,30 @@ function PaymentsCreate(props) {
               value={tenant_id}
               onChange={(e) => setTenant_id(e.target.value)}
             >
-              {tenantData.map((tenant, index) => <option key={index} value={tenant._id}>{tenant.firstName} {tenant.lastName}</option>)}
-              {/* <option>65538d7b3fabec24e4725638</option>
-              <option>6553cc7f8f916efb8ba24a60</option>
-              <option>6553d01d8f916efb8ba24a73</option>
-              <option>6553d6e08f916efb8ba24a78</option> */}
+              {tenantData.map((tenant, index) => (
+                <option key={index} value={tenant._id}>
+                  {tenant.firstName} {tenant.lastName}
+                </option>
+              ))}
             </Input>
           </FormGroup>
 
           {/* Select Amount */}
           <FormGroup>
             <Label for="selectAmount">Enter $ Amount</Label>
-            <Input 
+            <Input
               id="selectAmount"
               name="selectAmount"
               type="number"
               value={amount}
               onChange={(e) => setAmount(e.target.value)}
-            >
-            </Input>
+            ></Input>
           </FormGroup>
 
           {/* Select Date */}
           <FormGroup>
             <Label for="selectDate">Select Date</Label>
-            <Input 
+            <Input
               id="selectDate"
               name="selectDate"
               type="date"
@@ -244,7 +198,7 @@ function PaymentsCreate(props) {
           {/* Select Payment State (Full, Partial etc.) */}
           <FormGroup>
             <Label for="selectPaymentState">Select Type of Payment</Label>
-            <Input 
+            <Input
               id="selectPaymentState"
               name="selectPaymentState"
               type="select"
@@ -261,13 +215,10 @@ function PaymentsCreate(props) {
 
           {/* Create Payment Button */}
           <Button onClick={handleSubmit}>Create Payment</Button>
-
         </Form>
-
       </div>
     </>
   );
 }
-
 
 export default PaymentsCreate;
